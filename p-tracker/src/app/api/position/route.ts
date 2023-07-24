975ba46a214dd3df3f9a7f9f266e5687bb704876
Melvin2306 <melvin.rinkleff@gmx.de>
2023-07-24 19:47:51 +0200

    deploy

--- p-tracker/src/app/api/position/route.ts
+++ p-tracker/src/app/api/position/route.ts
@@ -5,8 +5,14 @@ export async function GET(request: Request) {
   try {
     const body = JSON.stringify({ id });
     return new Response(body, { headers });
-  } catch (err) {
-    return new Response(JSON.stringify({ error: err.message }), {
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return new Response(JSON.stringify({ error: err.message }), {
+        status: 500,
+        headers,
+      });
+    }
+    return new Response(JSON.stringify({ error: "Unknown error" }), {
       status: 500,
       headers,
     });
