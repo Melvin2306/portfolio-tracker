07efaa0f54d46c9aacbee010f14412625e6e804d
Melvin2306 <melvin.rinkleff@gmx.de>
2023-07-25 10:59:40 +0200

    Update NavBar.tsx

--- p-tracker/src/app/components/NavBar.tsx
+++ p-tracker/src/app/components/NavBar.tsx
@@ -2,18 +2,19 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 p-6">
+    <nav className="flex items-right justify-between bg-gradient-to-r from-blue-400 via-purple-500 to-pink-500 p-6">
       <div></div>
       <div></div>
       <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <Link href="/">Overview</Link>
-      <Link href="/positions">Portfolio</Link>
-      <Link href="/transactions">Transactions</Link>
+      <Link href="/" className="ml-auto mr-5">
+        Overview
+      </Link>
+      <Link href="/positions" className="mr-5">
+        Portfolio
+      </Link>
+      <Link href="/transactions" className="mr-5">
+        Transactions
+      </Link>
     </nav>
   );
 }
