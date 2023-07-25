e167cafbdf0d44feb9b0f535f7253dbdfe41dc16
Melvin2306 <melvin.rinkleff@gmx.de>
2023-07-25 11:56:00 +0200

    fixes index page

--- p-tracker/src/app/components/PositionsTable.tsx
+++ p-tracker/src/app/components/PositionsTable.tsx
@@ -1,13 +1,1 @@
-import prisma from "../lib/prisma";
-
-export const getStaticProps: GetStaticProps = async () => {
-  const feed: any = await prisma.position.findMany({
-    where: { userId: 1 },
-    include: {
-      transactions: true,
-    },
-  });
-  return {
-    props: { feed },
-  };
-};
+export default function PositionsTable() {}
