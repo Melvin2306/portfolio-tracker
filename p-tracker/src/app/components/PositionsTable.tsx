ebf2346206de29976dae0e94e6393c2fbeba9506
Melvin2306 <melvin.rinkleff@gmx.de>
2023-07-27 19:49:56 +0200

    First Get and average price in frontend

--- p-tracker/src/app/components/PositionsTable.tsx
+++ p-tracker/src/app/components/PositionsTable.tsx
@@ -1,1 +1,50 @@
-export default function PositionsTable() {}
+"use client";
+import React, { useEffect, useState } from "react";
+
+export default function PositionsTable() {
+  const [positions, setPositions] = useState([]);
+
+  useEffect(() => {
+    fetchPositions();
+  }, []);
+
+  async function fetchPositions() {
+    const positions = await fetch("/api/position").then((res) => res.json());
+    setPositions(positions);
+  }
+
+  let averagePrice;
+
+  averagePrice = (positions) => {
+    let sum = 0;
+    let totalQuantity = 0;
+    for (let i = 0; i < positions.length; i++) {
+      sum += positions[i].price * positions[i].quantity;
+      totalQuantity += positions[i].quantity;
+    }
+    return sum / totalQuantity;
+  };
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Position</th>
+          <th>Price</th>
+          <th>Quantity</th>
+          <th>Average Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {positions.map((position) => (
+          <tr key={position.id}>
+            <td>{position.name}</td>
+            <td>{position.price}</td>
+            <td>{position.quantity}</td>
+            <td>{averagePrice(positions)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
