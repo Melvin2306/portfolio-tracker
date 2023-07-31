9126a85a285c6f72103fb4655ce70ba532c93759
Melvin2306 <melvin.rinkleff@gmx.de>
2023-07-31 16:06:23 +0200

    current price

--- p-tracker/src/app/components/PositionsTable.tsx
+++ p-tracker/src/app/components/PositionsTable.tsx
@@ -1,17 +1,46 @@
 "use client";
+
+import { toast } from "react-toastify";
 import React, { useEffect, useState } from "react";
+import twelvedata from "twelvedata";
 
 export default function PositionsTable() {
   const [positions, setPositions] = useState([]);
-
-  useEffect(() => {
-    fetchPositions();
-  }, []);
+  const [currentPrice, setCurrentPrice] = useState(0);
+  const [error, setError] = useState(null);
 
   async function fetchPositions() {
     const positions = await fetch("/api/position").then((res) => res.json());
     setPositions(positions);
   }
+
+  /// Twelvedata API ///
+
+  // configure the client with your API key
+
+  // initialize and use the client
+
+  const client = twelvedata(config);
+
+  // time series
+
+  console.log(positions);
+
+  let params = {
+    symbol: "AAPL",
+    interval: "1min",
+    outputsize: 1,
+  };
+
+  client
+    .timeSeries(params)
+    .then((data) => {
+      setCurrentPrice(data.values[0].close);
+    })
+    .catch((error) => {
+      setError(error);
+      console.log(error);
+    });
 
   let averagePrice;
 
@@ -25,6 +54,14 @@ export default function PositionsTable() {
     return sum / totalQuantity;
   };
 
+  if (error || !positions || !currentPrice) {
+    toast(error, { type: "error" });
+  }
+
+  useEffect(() => {
+    fetchPositions();
+  }, []);
+
   return (
     <table>
       <thead>
@@ -33,6 +70,7 @@ export default function PositionsTable() {
           <th>Price</th>
           <th>Quantity</th>
           <th>Average Price</th>
+          <th>Current Price</th>
         </tr>
       </thead>
       <tbody>
@@ -42,6 +80,7 @@ export default function PositionsTable() {
             <td>{position.price}</td>
             <td>{position.quantity}</td>
             <td>{averagePrice(positions)}</td>
+            <td>{currentPrice}</td>
           </tr>
         ))}
       </tbody>
